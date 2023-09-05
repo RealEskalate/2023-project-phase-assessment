ced34e0dfccd779dbe0bee66aa4a2c1778a1f6ef
ffekirnew <ffekirnew0808@gmail.com>
2023-09-05 10:26:51 +0300

    feat(backend.fikernew-birhanu.assessment): Fix MediatR version and add DI package

--- backend/BackendAssessment/Domain/Entites/Product.cs
+++ backend/BackendAssessment/Domain/Entites/Product.cs
@@ -7,6 +7,8 @@ public class Product : BaseEntity
     public string Name { get; set; } = null!;
     public int Price { get; set; }
     public int Stock { get; set; }
+    public int UserId { get; set; }
 
+    public virtual User User { get; set; } = null!;
     public virtual ICollection<Category> Categories { get; set; } = null!;
 }
