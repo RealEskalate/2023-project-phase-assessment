c618fad662ec47ade1653b2357642d356ca543e4
Ananya <afmtrendaid@gmail.com>
2023-09-05 01:14:29 -0700

    Entities, DTOs and DBContext

---
+++ backend/BackendAssessment/Persistence/PersistenceConfigurationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{
+    public static class PersistenceConfigurationService
+    {
+
+        public static IServiceCollection AddPersistanceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ProductHubDbContext>(options => {
+                options.UseNpgsql(configuration.GetConnectionString("ProductHub"));
+            });
+
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            //services.AddScoped<IUserRepository, UserRepository>();
+            //services.AddScoped<IPostRepository, PostRepository>();
+            //services.AddScoped<IFollowRepository, FollowRepository>();
+            //services.AddScoped<ILikeRepository, LikeRepository>();
+            return services;
+        }
+
+    }
+}
+
