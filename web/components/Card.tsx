ae331c516eab804135eb573c62f81c52e1712738
Naol Taye <naoltd164@gmail.com>
2023-09-05 09:08:41 +0300

    Updated card component

--- web/components/Card.tsx
+++ web/components/Card.tsx
@@ -1,20 +1,30 @@
-export default function Card() {
+interface CardProps {
+  _id: string;
+  fullName: string;
+  speciality?: [name: string];
+  institutionID_list?: [institutionName: string];
+  photo: string;
+}
+export default function Card({
+  _id,
+  fullName,
+  speciality,
+  institutionID_list,
+  photo,
+}: CardProps) {
   return (
     <div className="w-[19.25rem] h-[16.125rem]">
-      <img
-        className=""
-        src="/Header/profile-picture.png"
-        alt="profile-picture"
-      />
-      <div className="relative">
+      {/* <div className="relative">
         <div className="absolute inset-x-0 bottom-0 h-1/2 bg-red-500 z-10"></div>
         <div className="h-24 bg-blue-500">Parent</div>
-      </div>
+      </div> */}
 
-      <div>picture</div>
-      <div>name</div>
-      <div>specialization</div>
-      <div>work place</div>
+      <div>
+        <img className="" src={photo} alt="profile-picture" />
+      </div>
+      <div>{fullName}</div>
+      <div>{speciality}</div>
+      <div>{institutionID_list}</div>
     </div>
   );
 }
