31882e4de18b545615ca62c79c4e8ab087470a31
Leul Wujira <meraf000@gmail.com>
2023-09-05 10:20:03 +0300

    feat: add movie entity

---
+++ mobile/lib/features/movie/domain/entities/movie.dart
@@ -0,0 +1,26 @@
+abstract class Equatable {}
+
+class Movie extends Equatable {
+    final String id;
+    final String title;
+    final String description;
+    final String duration;
+    final String image;
+    final String category;
+    final double rating;
+    final Datetime createdAt;
+
+    const Movie({
+        required this.id,
+        required this.title,
+        required this.description,
+        required this.duration,
+        required this.image,
+        required this.category,
+        required this.rating,
+        required this.createdAt,
+    });
+
+    @override
+    List<Object?> get props => [id, title, description, duration, image, category];
+}
