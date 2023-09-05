73520dde23ed2a8315847956735abf2e4e3a6333
Leul Wujira <meraf000@gmail.com>
2023-09-05 15:31:59 +0300

    feat: add static text content

---
+++ mobile/lib/core/presentation/content/app_text_content.dart
@@ -0,0 +1,9 @@
+abstract final class AppTextContent {
+  static final onboardingScreen = [
+    {
+      'title': 'Streaming movie and TV. Watch instantly',
+      'description':
+          'Enjoy all our favorite films and TV shows on your streaming devices'
+    },
+  ];
+}
