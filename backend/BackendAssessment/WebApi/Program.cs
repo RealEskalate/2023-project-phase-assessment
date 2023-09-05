b666781a16b00001baf1549bfc2d2ef5c6991adc
= <amanuel.tewodros@a2sv.org>
2023-09-05 16:30:03 +0300

    implement the api layer for product feature

--- backend/BackendAssessment/WebApi/Program.cs
+++ backend/BackendAssessment/WebApi/Program.cs
@@ -1,12 +1,55 @@
+using Microsoft.OpenApi.Models;
+using ProductHub.Application;
+using ProductHub.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// User services
+// builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+// Services Registration
+builder.Services
+    .AddApplication()
+    .AddPersistence(builder.Configuration);
+builder.Services.AddSwaggerGen(
+    c =>  {
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+    }
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,9 +60,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
