bbc01fb99c53a9122ddcc3c574edc88556fafe8b
Leul Wujira <meraf000@gmail.com>
2023-09-05 15:33:08 +0300

    feat: add onboarding screen text content widget

---
+++ mobile/lib/features/onboarding/presentation/widgets/onboarding_content.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/presentation/theme/app_colors.dart';
+import '../../../../core/presentation/theme/app_theme.dart';
+
+class OnboardingContent extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const OnboardingContent({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: AppDimensions.width,
+      height: 380.h,
+      decoration: const BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(60),
+          topRight: Radius.circular(60),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 25, bottom: 0, left: 25, right: 25),
+        child: Column(
+          children: [
+            Flexible(
+              child: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+            ),
+            SizedBox(height: 30.h),
+            Text(
+              description,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            SizedBox(height: 50.h),
+            SizedBox(
+              height: 50.h,
+              width: 360.w,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Get Started'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
