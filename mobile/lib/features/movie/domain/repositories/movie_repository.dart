f77be459fd500777da500bfcb072c7a6ab406084
betabravah <betabravah@gmail.com>
2023-09-05 10:49:47 +0300

    feat(AAiT-mobile-betty-yemane): Make Usecases camelCase

--- mobile/lib/features/movie/domain/repositories/movie_repository.dart
+++ mobile/lib/features/movie/domain/repositories/movie_repository.dart
@@ -7,7 +7,7 @@ abstract class MovieRepository {
   Future<Either<Failure, List<Movie>>> getMovies();
   Future<Either<Failure, Movie>> getMovie(String id);
   Future<Either<Failure, List<Movie>>> filterMovies(String query);
-  Future<Either<Failure, Movie>> bookmarkMovie(Movie movie);
-  Future<Either<Failure, Movie>> unbookmarkMovie(Movie movie);
+  Future<Either<Failure, Movie>> bookMarkMovie(Movie movie);
+  Future<Either<Failure, Movie>> unBookMarkMovie(Movie movie);
   Future<Either<Failure, List<Movie>>> getBookmarkedMovies();
 }
