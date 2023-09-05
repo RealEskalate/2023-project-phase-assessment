c6054b8fca02ae636dbf2c785134acc0e3b9dbcd
Ananiya <ananiyat24@gmail.com>
2023-09-05 18:30:40 +0300

    add exceptions

---
+++ backend/BackendAssessment/Application/Exceptions/ServerErrorException.cs
@@ -0,0 +1,9 @@
+namespace Application.Exceptions;
+
+public class ServerErrorException : ApplicationException
+{
+    public ServerErrorException(string message) : base(message)
+    {
+        
+    }
+}
