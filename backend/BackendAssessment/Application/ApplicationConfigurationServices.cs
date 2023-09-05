d04d68317c69646f13a21fbf411b354358e8e590
Ananya <afmtrendaid@gmail.com>
2023-09-05 06:10:52 -0700

    Working Model

--- backend/BackendAssessment/Application/ApplicationConfigurationServices.cs
+++ backend/BackendAssessment/Application/ApplicationConfigurationServices.cs
@@ -10,6 +10,8 @@ public static class ApplicationConfigurationServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
         return services;
     }
 
