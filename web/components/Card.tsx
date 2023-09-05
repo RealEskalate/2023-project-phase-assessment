0c32c4777bbaf7c0da13171534c3dcbb78286b15
Naol Taye <naoltd164@gmail.com>
2023-09-05 05:01:02 +0300

    Created basic card component

---
+++ web/components/Card.tsx
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <div className="w-[19.25rem] h-[16.125rem]">
+      <div>picture</div>
+      <div>name</div>
+      <div>specialization</div>
+      <div>work place</div>
+    </div>
+  );
+}
