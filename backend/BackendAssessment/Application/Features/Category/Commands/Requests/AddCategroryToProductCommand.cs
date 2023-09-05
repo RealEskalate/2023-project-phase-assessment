f36f0d75d56765c1552a21ab67befbbd099697cc
Geleta116 <geleta.dabakumsa@gmail.com>
2023-09-05 18:07:17 +0300

    added the add category feature

--- backend/BackendAssessment/Application/Features/Category/Commands/Requests/AddCategroryToProductCommand.cs
+++ backend/BackendAssessment/Application/Features/Category/Commands/Requests/AddCategroryToProductCommand.cs
@@ -1,9 +1,10 @@
 using BackendAssessment.Application.Common.Responses;
+using BackendAssessment.Application.Features.Category.DTO;
 using MediatR;
 
 namespace BackendAssessment.Application.Features.Category.Commands.Requests;
 
-public class AddCategroryToProductCommand : IRequest<CommonResponse<int>> 
+public class AddCategroryToProductCommand : IRequest<CommonResponse<int>>
 {
-    public 
+    public AddCategoryDto addCategoryDto;
 }
