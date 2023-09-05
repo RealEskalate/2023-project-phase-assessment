1f114df22b637169cc5305e336627f319f34f7bc
Geleta116 <geleta.dabakumsa@gmail.com>
2023-09-05 16:51:12 +0300

    added the cqrs for product

--- backend/BackendAssessment/Application/Features/Product/Queries/Requests/GetProductByIdRequest.cs
+++ backend/BackendAssessment/Application/Features/Product/Queries/Requests/GetProductByIdRequest.cs
@@ -1,6 +1,11 @@
+using BackendAssessment.Application.Common.Responses;
+using BackendAssessment.Application.Features.Product.DTO;
+using MediatR;
+using BackendAssessment.Domain.Entities;
+
 namespace BackendAssessment.Application.Features.Product.Queries.Requests;
 
-public class GetProductByIdRequest
+public class GetProductByIdRequest : IRequest<CommonResponse<CreateProductDto>>
 {
-    
+    public int Id {get; set;}
 }
