618e3a6469eb218932615e387fe6f7e79f2229c2
ffekirnew <ffekirnew0808@gmail.com>
2023-09-05 15:08:26 +0300

    Add category mapping profiles

---
+++ backend/BackendAssessment/Application/Profiles/MappingProfiles.cs
@@ -0,0 +1,18 @@
+using Application.Features.Categories.Dtos;
+using Application.Features.Products.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ProductDto, Product>();
+        CreateMap<Product, ProductDto>();
+
+        CreateMap<Category, CategoryDto>();
+        CreateMap<CategoryDto, Category>();
+    }
+}
