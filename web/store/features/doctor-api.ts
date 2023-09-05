b94a6f1553878c46523da66f8466e47be0cbeafb
Naol Taye <naoltd164@gmail.com>
2023-09-05 09:01:09 +0300

    Added useGetAllDoctorsMutation

--- web/store/features/doctor-api.ts
+++ web/store/features/doctor-api.ts
@@ -1,7 +1,8 @@
 import { doctorDetail } from "@/types/doctor-detail.model";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const Url = "https://hakimhub-api-dev-wtupbmwpnq-uc.a.run.app/api/v1";
-export const doctorApi = createApi({
+//hakimhub-api-dev-wtupbmwpnq-uc.a.run.app/api/v1/
+https: export const doctorApi = createApi({
   reducerPath: "doctorApi",
   baseQuery: fetchBaseQuery({
     baseUrl: Url,
@@ -10,7 +11,11 @@ export const doctorApi = createApi({
     getDoctorDetails: build.query<doctorDetail, string>({
       query: (id) => `/users/doctorProfile/${id}`,
     }),
+    getAllDoctors: build.mutation<{ find: any; page: any }, doctorDetail[]>({
+      query: (find: any, page: any) =>
+        `/search=${find}?institutions=false&from=${page}&size=8`,
+    }),
   }),
 });
 
-export const { useGetDoctorDetailsQuery } = doctorApi;
+export const { useGetDoctorDetailsQuery, useGetAllDoctorsMutation } = doctorApi;
