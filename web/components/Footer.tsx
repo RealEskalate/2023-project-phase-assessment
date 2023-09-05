2cc2ed33c1e99e92fc27972f05249abc0a5180aa
Naol Taye <naoltd164@gmail.com>
2023-09-05 04:44:36 +0300

    Implemented basic footer layout

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -1,3 +1,41 @@
-export default funtion Footer(){
-    return <></>
+export default function Footer() {
+  // todo: apply ui and icos
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>HakimHub</div>
+        <div className="flex justify-between">
+          <div>
+            <p>get connected</p>
+            <p>For Physicians</p>
+            <p>For Hospitals</p>
+          </div>
+          <div>
+            <p>Actions</p>
+            <p>Find a doctor</p>
+            <p>Find a hospital</p>
+          </div>
+          <div>
+            <p>Company</p>
+            <p>About Us</p>
+            <p>Career</p>
+            <p>Join our team</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="flex justify-between">
+        <div className="flex justify-between">
+          <p>Privacy Policy</p>
+          <p>Terms of Use</p>
+        </div>
+        <div className="flex justify-between">
+          <p>socials</p>
+          <p>socials</p>
+          <p>socials</p>
+          <p>socials</p>
+        </div>
+      </div>
+    </div>
+  );
 }
