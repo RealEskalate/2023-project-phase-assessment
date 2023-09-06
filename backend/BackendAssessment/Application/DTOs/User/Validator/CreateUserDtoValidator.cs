a799a76ca215715fc8243052f677709adfc12bc4
Aymen Eliyas <aymen.eliyas@a2sv.org>
2023-09-06 09:28:43 +0300

    initial

---
+++ backend/BackendAssessment/Application/DTOs/User/Validator/CreateUserDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.User.Validator
+{
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        public CreateUserDtoValidator() {
+
+            RuleFor(dto => dto.username)
+                .NotEmpty()
+                .WithMessage("name is required.");
+
+            RuleFor(dto => dto.email)
+                .NotEmpty()
+                .WithMessage("email is required.");
+
+            RuleFor(dto => dto.password)
+                .NotEmpty()
+                .WithMessage("password is required.");
+        
+        }
+    }
+}
