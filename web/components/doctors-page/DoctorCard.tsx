b784de5d217de1aece09e45b54eca2bda06013a6
Yakin Teshome <yakin16tm@gmail.com>
2023-09-06 13:51:05 +0300

    doctors page fix

--- web/components/doctors-page/DoctorCard.tsx
+++ web/components/doctors-page/DoctorCard.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 const DoctorCard = ({ doctor }) => {
 
     return (
-        <div className='flex flex-col p-3 shadow-lg rounded-xl w-[300px] font-poppins'>
+        <div className='flex flex-col justify-center p-3 shadow-lg rounded-xl w-[300px] font-poppins'>
             <div className='relative w-[90px] h-[90px] rounded-[45px] border-primary border-3 bg-gray-400 mx-auto'>
-            <Image src={doctor.photo} alt='profilr photo' className='absolute' objectFit='cover' fill={true}/>
+            <Image src={doctor.photo} alt='profilr photo' className='absolute rounded-full' objectFit='contain' fill={true}/>
             </div>
-            <h3 className='mt-3 font-semibold text-[18px]'>{doctor.fullName}</h3>
-            <div className='mx-auto bg-primary text-white rounded-full text-lg w-fit px-4 py-3'>{doctor.speciality.name}</div>
-            <p className='mt-7 text-center text-[#686868] text-lg'>{doctor.institutionID_list.institutionName}</p>
+            <h3 className='mt-3 w-full font-semibold text-[18px] text-center'>{doctor.fullName}</h3>
+            <div className='mx-auto bg-primary text-white rounded-full text-lg w-fit px-4 py-3'>{doctor.speciality[0].name}</div>
+            <p className='mt-7 text-center text-[#686868] text-lg'>{doctor.institutionID_list[0].institutionName}</p>
         </div>
     )
 }
--- web/components/header/Header.tsx
+++ web/components/header/Header.tsx
@@ -6,11 +6,11 @@ const Header: React.FC = () => {
         <div className='w-screen  mt-0 py-5 px-20'>
             <nav className="w-full flex justify-between">
                 <div className='relative'>
-                    <Image src='/assets/img/hakim-logo.png' alt='logo image' className='absolute' objectFit='cover' fill={true} />
+                    <Image src='/assets/img/hakim-logo.png' alt='logo image' className='absolute z-10' objectFit='cover' fill={true} />
                 </div>
                 <div className='flex gap-2'>
                     <div className='relative'>
-                        <Image src='/assets/img/hakim-account.png' alt='account image'className='absolute' objectFit='cover' fill={true} />
+                        <Image src='/assets/img/hakim-account.png' alt='account image'className='absolute z-10' objectFit='cover' fill={true} />
                     </div>
                     <div className=''>Yakin Teshome </div>
                 </div>
